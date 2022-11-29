a59bebfcde4c93b6318c2218177e19db1bcc5ed8
mhaciyev17623 <118837555+mhaciyev17623@users.noreply.github.com>
2022-11-29 23:42:55 +0400

    Update BaseFunctions.js

--- BaseFunctions.js
+++ BaseFunctions.js
@@ -1,26 +1,26 @@
 function Ada() {
-    document.getElementById("Ada").style.display = "block";
+    document.getElementById("Ada0").style.display = "block";
     document.getElementById("CodeEvent").style.display = "none";
     document.getElementById("Team").style.display = "none";
     document.getElementById("Project").style.display = "none";
 }
 
 function CodeEvent() {
-    document.getElementById("Ada").style.display = "none";
+    document.getElementById("Ada0").style.display = "none";
     document.getElementById("CodeEvent").style.display = "block";
     document.getElementById("Team").style.display = "none";
     document.getElementById("Project").style.display = "none";
 }
 
 function Team() {
-    document.getElementById("Ada").style.display = "none";
+    document.getElementById("Ada0").style.display = "none";
     document.getElementById("CodeEvent").style.display = "none";
     document.getElementById("Team").style.display = "block";
     document.getElementById("Project").style.display = "none";
 }
 
 function Project() {
-    document.getElementById("Ada").style.display = "none";
+    document.getElementById("Ada0").style.display = "none";
     document.getElementById("CodeEvent").style.display = "none";
     document.getElementById("Team").style.display = "none";
     document.getElementById("Project").style.display = "block";
